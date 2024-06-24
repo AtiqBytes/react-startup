40bf6186ecc4ae2b56897bfc6a44200b67849cc3
AtiqBytes <atiqbytes@gmail.com>
2024-06-24 12:34:24 +0500

    counter project + states (basic)

--- 01basicvite/src/App.jsx
+++ 01basicvite/src/App.jsx
@@ -1,15 +1,17 @@
 import Youtube from "./Youtube"
 
 function App() {
- 
+
+ const username='atiq'
 
   return (
     <>
-   <h1>vite react app</h1>
-   <h1>vite react app</h1>
+   <h1>vite react app {2+2}</h1>
+   <h1>vite react app {username}</h1>
    <Youtube />
    </>
   )
 }
 
 export default App
+ 
