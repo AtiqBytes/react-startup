f8607a96684c27cf6bb351788fb2ce1b35a51190
AtiqBytes <atiqbytes@gmail.com>
2024-06-25 11:15:06 +0500

    part 2 completed

--- 04bgChanger/src/App.jsx
+++ 04bgChanger/src/App.jsx
@@ -4,17 +4,17 @@ import "./App.css";
 function App() {
  const[color,setColor]=useState('olive')
 
- function changeColor(color){
-    setColor(color)
- }
+//  function changeColor(color){
+//     setColor(color)
+//  }
 
   return (
     <div className="w-full h-screen duration-200 bg-black" style={{backgroundColor:color}}>
       <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
         <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
 
-          <button onClick={()=>changeColor('red')} className="outline-none px-4 py-1 rounded-full text-white shadow-lg text-black">Red</button>
-          <button onClick={()=>changeColor('green')} className="outline-none px-4 py-1 rounded-full text-white shadow-lg text-black">Green</button>
+          <button onClick={()=>setColor('red')} style={{backgroundColor:'red'}} className="outline-none px-4 py-1 rounded-full text-white shadow-lg text-black">Red</button>
+          <button onClick={()=>setColor('green')} style={{backgroundColor:'green'}} className="outline-none px-4 py-1 rounded-full text-white shadow-lg text-black">Green</button>
          
         </div>
       </div>
