4ff04037351f63b7b3c42a7cab3953a2971de334
AtiqBytes <atiqbytes@gmail.com>
2024-07-06 10:13:06 +0500

    filling out html in about,contact,footer,header,home,user components

---
+++ 07reactrouter/src/components/User/User.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function User() {
+  return (
+    <div className='bg-orange-500 text-black text-3xl text-center'>User</div>
+  )
+}
+
+export default User
--- 07reactrouter/src/main.jsx
+++ 07reactrouter/src/main.jsx
@@ -11,12 +11,16 @@ import Layout from "./Layout.jsx";
 import Home from "./components/Home/Home.jsx";
 import React from "react";
 import About from "./components/About/About.jsx";
+import Contact from "./components/Contacts/Contact.jsx";
+import User from "./components/User/User.jsx";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<Layout />}>
       <Route path="" element={<Home />} />
       <Route path="about" element={<About/>}/>
+      <Route path="contact" element={<Contact/>}/>
+      <Route path="user" element={<User/>}/>
 
     </Route>
   )
